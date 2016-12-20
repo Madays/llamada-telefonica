23e1558cf6db11dcdcf3cfd4ac3abed04ac6aada
Madays <maday.choque@gmail.com>
2016-12-20 11:20:34 -0500

    trabajando con  botons

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-var minUsadosLlamada, precioMinuto, precioLLamada;
-alert("Calculando el pago de llamada telefónica")
-minUsadosLlamada = prompt("Por favor, minutos usados en llamada");
-precioMinuto = prompt("Por favor, ingresa el precio por minuto ");
+    /*var minUsadosLlamada, precioMinuto, precioLLamada;
+    alert("Calculando el pago de llamada telefónica");
+    minUsadosLlamada = prompt("Por favor, minutos usados en llamada");
+    precioMinuto = prompt("Por favor, ingresa el precio por minuto ");
 
-precioLLamada = parseFloat(minUsadosLlamada)*parseFloat(precioMinuto);
+    precioLLamada = parseFloat(minUsadosLlamada)*parseFloat(precioMinuto);
 
-alert("La llamada le cuesta: " + precioLLamada + " soles");
+    alert("La llamada le cuesta: " + precioLLamada + " soles");*/
+    var minUsadosLlamada, precioMinuto, precioLLamada;
+function ingreseDatos() {
+    minUsadosLlamada = prompt("Por favor, minutos usados en llamada");
+    precioMinuto = prompt("Por favor, ingresa el precio por minuto ");
+}
+function calcularPrecioLlamada(){
+    precioLLamada = parseFloat(minUsadosLlamada)*parseFloat(precioMinuto);
+    alert(precioLLamada);
+}
+
